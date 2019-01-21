4d6221404bceed72f43eb5996767ceaba3bd4c40
ratnadeepbiswakarma <ratnadeeppersonal@gmail.com>
2019-01-21 14:09:47 +0530

    image background added from server returned data

--- src/components/Card/Sprite/Sprite.jsx
+++ src/components/Card/Sprite/Sprite.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 const Sprite = props => {
-    let card = props.card;
+  let card = props.card;
+  let cardColors = card.cardColors;
   return (
     <div className="photo-container">
-      <img src={card.sprite} alt="Pokemon" />
+      <img
+        style={{
+          backgroundColor: card.cardColors.imgbg
+        }}
+        src={card.sprite}
+        alt="Pokemon"
+      />
     </div>
   );
 };
